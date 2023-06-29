b91db1c4a3db21aad5afa1fef73ebbd2b47ba719
zzh <zzh@youxiake.com>
2023-06-29 10:42:26 +0800

    1

--- IJPay-Demo-SpringBoot/src/main/java/com/ijpay/demo/config/StartupRunner.java
+++ IJPay-Demo-SpringBoot/src/main/java/com/ijpay/demo/config/StartupRunner.java
@@ -4,12 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
 /**
  * @author juzi
  * @date 2023/6/28 下午 6:19
  * @description Spring 容器在启动后，会自动调用执行 run 方法。并且在整个应用生命周期内只会执行一次。
  */
 @Order(1)
+@Component
 public class StartupRunner implements CommandLineRunner {
 	private static final Logger logger = LoggerFactory.getLogger(StartupRunner.class);
 
